c0299db3d7bb4ad535fc4b69b8f3cc20aae62482
kim dong kyung <kdkd422@naver.com>
2024-12-12 14:52:46 +0900

    Feat User서비스 에러 타입

--- BasketballGuest/BasketballGuest/Core/Service/UserService/UserError.swift
+++ BasketballGuest/BasketballGuest/Core/Service/UserService/UserError.swift
@@ -7,9 +7,28 @@
 
 import Foundation
 
-enum UserError: Error {
-    case network
+enum UserError: Error, LocalizedError {
+    case userNotFound
     case invalidate
-    case Encode
-    case Decode
+    case decodingError(Error)
+    case networkError(Error)
+    case permissionDenied
+    case unknownError(Error)
+    
+    var errorDescription: String? {
+        switch self {
+        case .userNotFound:
+            return "사용자 정보를 찾을 수 없습니다."
+        case .decodingError(let error):
+            return "사용자 정보를 해석하는 데 실패했습니다: \(error.localizedDescription)"
+        case .networkError(let error):
+            return "네트워크 오류가 발생했습니다: \(error.localizedDescription)"
+        case .permissionDenied:
+            return "위치 권한이 거부되었습니다."
+        case .unknownError(let error):
+            return "알 수 없는 오류가 발생했습니다: \(error.localizedDescription)"
+        case .invalidate:
+            return "로그인 정보를 잃었습니다."
+        }
+    }
 }
