13e8a014e449e7d2364911f23ac868c3dff2cd6e
kim dong kyung <kdkd422@naver.com>
2024-12-25 15:15:36 +0900

    Add 유저 프로필 컴포넌트

--- BasketballGuest/BasketballGuest/Utils/Component/Guest/UserProfileView.swift
+++ BasketballGuest/BasketballGuest/Utils/Component/Guest/UserProfileView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct UserProfileView: View {
+    
+    var userProfile: String?
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
