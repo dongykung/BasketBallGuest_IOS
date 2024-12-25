d08232d3dd9bb90dbc53c2e7d0629ff5bd673759
kim dong kyung <kdkd422@naver.com>
2024-12-25 15:15:02 +0900

    Add MapView 컴포넌트

--- BasketballGuest/BasketballGuest/Utils/Component/Guest/MapView.swift
+++ BasketballGuest/BasketballGuest/Utils/Component/Guest/MapView.swift
@@ -6,13 +6,41 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    
+    @State private var region: MKCoordinateRegion
+    @State private var annotationItems: [Location]
+    init(center: CLLocationCoordinate2D, annotationItems: [Location]) {
+        self.region = MKCoordinateRegion(center: center, span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        self.annotationItems = annotationItems
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Map(coordinateRegion: $region, interactionModes: .all, showsUserLocation: true, annotationItems: annotationItems, annotationContent: { location in
+            MapAnnotation(coordinate: location.coordinate) {
+                VStack {
+                    Image(systemName: "mappin.and.ellipse.circle.fill")
+                        .resizable()
+                        .frame(width: 24, height: 24)
+                        .foregroundColor(.red)
+                    Text(location.name)
+                        .font(.semibold16)
+                }
+            }
+        })
     }
 }
 
-#Preview {
-    MapView()
+struct Location: Identifiable {
+    var id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
 }
+
+#Preview {
+    MapView(center: .init(latitude: 37.33875766, longitude: 126.9364757), annotationItems: [.init(name: "바스농구", coordinate: .init(latitude: 37.33875766, longitude: 126.9364757))])
+}
+
+
