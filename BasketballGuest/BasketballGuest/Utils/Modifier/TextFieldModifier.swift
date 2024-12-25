eb61770cc219c378c629eb276adaea32ade743b7
kim dong kyung <kdkd422@naver.com>
2024-12-25 17:30:42 +0900

    Add 채팅 텍스트필드 모디파이어

--- BasketballGuest/BasketballGuest/Utils/Modifier/TextFieldModifier.swift
+++ BasketballGuest/BasketballGuest/Utils/Modifier/TextFieldModifier.swift
@@ -5,4 +5,16 @@
 //  Created by 김동경 on 12/25/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct TextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(4)
+            .overlay {
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(Color(uiColor: .systemGray5), lineWidth: 1)
+                    .foregroundStyle(.clear)
+            }
+    }
+}
