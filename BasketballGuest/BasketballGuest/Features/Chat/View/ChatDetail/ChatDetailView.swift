d41bc226c43473fc9d96315e11893b1cb74c3911
kim dong kyung <kdkd422@naver.com>
2024-12-25 15:14:37 +0900

    Add 채팅 뷰 및 뷰모델

--- BasketballGuest/BasketballGuest/Features/Chat/View/ChatDetail/ChatDetailView.swift
+++ BasketballGuest/BasketballGuest/Features/Chat/View/ChatDetail/ChatDetailView.swift
@@ -8,11 +8,103 @@
 import SwiftUI
 
 struct ChatDetailView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @StateObject private var viewModel: ChatDetailViewModel = .init()
+    private let chatUser: UserDTO
+    
+    init(chatUser: UserDTO) {
+        self.chatUser = chatUser
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ChatTopBar(user: chatUser) {
+                dismiss()
+            }
+            Divider()
+            switch viewModel.loadState {
+            case .none, .loading:
+                LoadingView()
+                    .transition(.opacity)
+            case .failed:
+                ErrorView {
+                    
+                }
+                .transition(.opacity)
+            case .completed:
+                ChatListView(text: $viewModel.text, chatList: viewModel.chats) { msg in
+                    
+                }
+                .transition(.opacity)
+            }
+        }
+        .toastView(toast: $viewModel.toast)
+        .animation(.smooth, value: viewModel.loadState)
+    }
+}
+
+fileprivate struct ChatListView: View {
+    
+    @State var height: CGFloat = 0
+    @Binding var text: String
+    let chatList: [Chat]
+    let send: (String) -> Void
+    
+    var body: some View {
+        VStack {
+            ScrollViewReader { proxy in
+                ScrollView {
+                    
+                }
+            }
+            
+            Spacer()
+            Divider()
+            HStack(alignment: .bottom) {
+                ResizableTextField(text: $text, height: $height)
+                    .frame(height: height < 80 ? height : 80)
+                    .overlay(alignment: .leading) {
+                        if text.isEmpty {
+                            Text("메시지를 입력해 주세요.")
+                                .transition(.opacity)
+                                .offset(x: 8)
+                                .foregroundStyle(.secondary)
+                        }
+                    }
+
+                Spacer()
+                Button {
+                    
+                } label: {
+                    Image(systemName: "paperplane.circle")
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+fileprivate struct ChatTopBar: View {
+    let user: UserDTO
+    let action: () -> Void
+    
+    var body: some View {
+        HStack {
+            Button(action: action) {
+                Image(systemName: "chevron.left")
+                    .foregroundStyle(.basic)
+                    .bold()
+            }
+            Spacer()
+            DefaultProfileView(profileUrl: user.profileImageUrl)
+            Text(user.nickName)
+            Spacer()
+        }
+        .padding(.horizontal)
     }
 }
 
 #Preview {
-    ChatDetailView()
+    ChatDetailView(chatUser: .init(nickName: "Dongkyung", positions: []))
 }
--- BasketballGuest/BasketballGuest/Features/Chat/ViewModel/ChatDetailViewModel.swift
+++ BasketballGuest/BasketballGuest/Features/Chat/ViewModel/ChatDetailViewModel.swift
@@ -6,3 +6,107 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+class ChatDetailViewModel: ObservableObject {
+    
+    private let db = Firestore.firestore()
+    var listener: ListenerRegistration?
+    var isExistChatRoom: Bool = false
+    
+    @Published var text: String = ""
+    @Published var chats: [Chat] = []
+    @Published var loadState: LoadState = .completed
+    @Published var toast: Toast?
+    
+    init() {
+        print("ChatDetailViewModel init")
+    }
+    
+    func initChatRoom(chatUserUid: String) async {
+        guard let myUid = Auth.auth().currentUser?.uid else {
+            await showToastMessage(msg: "로그인 정보를 잃었습니다 다시 로그인 해주세요.")
+            return
+        }
+        
+        let chatRoomId = generateChatRoomId(uid1: myUid, uid2: chatUserUid)
+        await updateLoadState(state: .loading)
+        
+        do {
+            //채팅방이 존재하면 채팅방의 Message를 Observe, 없다면 채팅방 생성
+            if try await isChatRoomExist(chatRoomId: chatRoomId) {
+                observeChatRoom(chatRoomId)
+            } else {
+                await updateisExistChatRoom(false)
+            }
+        } catch{
+            await updateLoadState(state: .failed)
+            await showToastMessage(msg: "채팅방 정보를 가져오는데 실패했습니다.")
+        }
+    }
+    
+    func isChatRoomExist(chatRoomId: String) async throws -> Bool {
+        do {
+            return try await db.collection("Chat").document(chatRoomId).getDocument().exists
+        } catch {
+            throw error
+        }
+    }
+    
+    func observeChatRoom(_ chatRoomId: String) {
+        self.listener = self.db.collection("Chat").document(chatRoomId).collection("Message").addSnapshotListener { [weak self] querySnapshot, error in
+            if let _ = error {
+                Task { @MainActor in
+                    self?.updateLoadState(state: .failed)
+                }
+            }
+            
+            guard let snapshot = querySnapshot else { return }
+            var newChats: [Chat] = []
+            
+            snapshot.documentChanges.forEach { diff in
+                do {
+                    let chat = try diff.document.data(as: Chat.self)
+                    newChats.append(chat)
+                } catch {
+                    
+                }
+            }
+            self?.addChatData(chat: newChats, chatRoomId: chatRoomId)
+        }
+    }
+    
+    func addChatData(chat: [Chat], chatRoomId: String) {
+        guard let myUid = Auth.auth().currentUser?.uid else { return }
+        
+        self.chats.append(contentsOf: chat)
+        
+        let _ = chat.filter { $0.sender != myUid && $0.readCount < MAXREADCOUNT}.map { chat in
+            self.db.collection("Chat").document(chatRoomId).collection("Message").document(chat.id ?? "").updateData(["readCount": FieldValue.increment(Int64(1))])
+        }
+    }
+    
+    @MainActor
+    func updateLoadState(state: LoadState) {
+        loadState = state
+    }
+    
+    @MainActor
+    func updateisExistChatRoom(_ exist: Bool) {
+        isExistChatRoom = exist
+    }
+    
+    @MainActor
+    func showToastMessage(msg: String) {
+        toast = Toast(style: .warning, message: msg)
+    }
+    
+    
+    deinit {
+        print("ChatDetailViewModel deinit")
+        listener?.remove()
+    }
+    
+    
+}
