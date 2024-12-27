cfad8eb29783dcc16ebd2c0159be8b62048d1e65
kim dong kyung <kdkd422@naver.com>
2024-12-27 13:28:43 +0900

    Add 채팅룸 모델

--- BasketballGuest/BasketballGuest/Core/Model/ChatRoom.swift
+++ BasketballGuest/BasketballGuest/Core/Model/ChatRoom.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+import FirebaseFirestore
+
+struct ChatRoom: Codable, Identifiable {
+    @DocumentID var id: String?
+    var participant: [String] // 대화 참가자들 UID (예: ["user1", "user2"])
+    var lastMessage: String
+    var lastMessageAt: Date
+    var readStatus: [String: Date] // 각 참가자의 마지막 읽은 메시지 시점
+}
