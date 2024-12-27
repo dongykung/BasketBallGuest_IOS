ae965eb190e8f032acdbdd4b4f1088699bf33839
kim dong kyung <kdkd422@naver.com>
2024-12-27 16:42:40 +0900

    Feat 내 채팅방 리스트 뷰

--- BasketballGuest/BasketballGuest/Features/Chat/View/ChatRoomList/ChatRoomListView.swift
+++ BasketballGuest/BasketballGuest/Features/Chat/View/ChatRoomList/ChatRoomListView.swift
@@ -8,11 +8,76 @@
 import SwiftUI
 
 struct ChatRoomListView: View {
+    @EnvironmentObject private var chatRoomStore: ChatRoomStore
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                switch chatRoomStore.loadState {
+                case .loading:
+                    LoadingView()
+                case .completed:
+                    LazyVStack {
+                        ForEach(chatRoomStore.chatRooms, id: \.chatRoom.id) { chatRoomWithUser in
+                            ChatListItemView(chatRoomWithUser: chatRoomWithUser)
+                                .padding(.vertical, 12)
+                        }
+                    }
+                case .none, .failed:
+                    ErrorView {
+                        chatRoomStore.observeChatRoomList()
+                    }
+                }
+            }
+            .navigationTitle("채팅")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+fileprivate struct ChatListItemView: View {
+    
+    let chatRoomWithUser: ChatRoomWithUser
+    
+    var body: some View {
+        NavigationLink {
+            ChatDetailView(chatUser: chatRoomWithUser.user)
+        } label: {
+            HStack {
+                DefaultProfileView(profileUrl: chatRoomWithUser.user.profileImageUrl, frame: 85)
+                    .padding(.trailing, 6)
+                VStack(alignment: .leading, spacing: 6) {
+                    HStack {
+                        Text(chatRoomWithUser.user.nickName)
+                            .foregroundStyle(.basic)
+                            .font(.semibold16)
+                        Spacer()
+                        Text(chatRoomWithUser.chatRoom.lastMessageAt.formattedTimeAgo)
+                            .foregroundStyle(.gray)
+                            .font(.regular14)
+                    }
+                    HStack {
+                        Text(chatRoomWithUser.chatRoom.lastMessage)
+                            .foregroundStyle(.gray)
+                            .font(.regular14)
+                        Spacer()
+                        if chatRoomWithUser.unReadCount > 0 {
+                            Text("\(chatRoomWithUser.unReadCount)")
+                                .bold()
+                                .font(.caption)
+                                .padding(6)
+                                .foregroundStyle(.white)
+                                .background(.red)
+                                .clipShape(.circle)
+                        }
+                    }
+                }
+            }
+            .padding()
+        }
     }
 }
 
 #Preview {
     ChatRoomListView()
+        .environmentObject(ChatRoomStore())
 }
