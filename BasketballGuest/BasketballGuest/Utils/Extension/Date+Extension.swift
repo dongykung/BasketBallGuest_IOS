c164491c7c8590c313bdda97deb82c33cd2aaa93
kim dong kyung <kdkd422@naver.com>
2024-12-27 16:41:47 +0900

    Add 채팅방 관련 날짜 텍스트 Extension 추가

--- BasketballGuest/BasketballGuest/Utils/Extension/Date+Extension.swift
+++ BasketballGuest/BasketballGuest/Utils/Extension/Date+Extension.swift
@@ -35,4 +35,42 @@ extension Date {
         formatter.dateFormat = "YYYY.MM.dd EEEE a h:mm"
         return formatter.string(from: self)
     }
+    
+    var chatSectionFormatted: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "YYYY년.MM월.dd일"
+        return formatter.string(from: self)
+    }
+    
+    var chatTimeFormatted: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "a h:mm"
+        return formatter.string(from: self)
+    }
+    
+    var formattedTimeAgo: String {
+        let now = Date()
+        let calendar = Calendar.current
+
+        let components = calendar.dateComponents([.minute, .hour, .day], from: self, to: now)
+
+        if let day = components.day, day > 0 {
+            if day == 1 {
+                return "어제"
+            } else {
+                let formatter = DateFormatter()
+                formatter.locale = Locale(identifier: "ko_KR")
+                formatter.dateFormat = "M월 d일 E요일"
+                return formatter.string(from: self)
+            }
+        } else if let hour = components.hour, hour > 0 {
+            return "\(hour)시간 전"
+        } else if let minute = components.minute, minute > 0 {
+            return "\(minute)분 전"
+        } else {
+            return "방금"
+        }
+    }
 }
