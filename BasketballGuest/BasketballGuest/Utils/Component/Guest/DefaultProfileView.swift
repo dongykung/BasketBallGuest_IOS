09bd3bd2ba405a9ed6e6d261e6a4386ab0c94e82
kim dong kyung <kdkd422@naver.com>
2024-12-27 16:41:24 +0900

    Fix 유저 프로파일 컴포넌트 사이즈 매개변수 추가

--- BasketballGuest/BasketballGuest/Utils/Component/Guest/DefaultProfileView.swift
+++ BasketballGuest/BasketballGuest/Utils/Component/Guest/DefaultProfileView.swift
@@ -9,6 +9,13 @@ import SwiftUI
 
 struct DefaultProfileView: View {
     let profileUrl: String?
+    let frame: CGFloat
+    
+    init(profileUrl: String?, frame: CGFloat = 40) {
+        self.profileUrl = profileUrl
+        self.frame = frame
+    }
+    
     var body: some View {
         if let profile = profileUrl {
             AsyncImage(url: URL(string: profile)) { phase in
@@ -16,21 +23,21 @@ struct DefaultProfileView: View {
                 case .empty:
                     Image(.user)
                         .resizable()
-                        .frame(width: 40, height: 40)
+                        .frame(width: frame, height: frame)
                 case .success(let image):
                     image
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .clipShape(.circle)
-                        .frame(width: 40, height: 40)
+                        .frame(width: frame, height: frame)
                 case .failure:
                     Image(.user)
                         .resizable()
-                        .frame(width: 40, height: 40)
+                        .frame(width: frame, height: frame)
                 @unknown default:
                     Image(.user)
                         .resizable()
-                        .frame(width: 40, height: 40)
+                        .frame(width: frame, height: frame)
                 }
             }
            
