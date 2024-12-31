4f52e95927e44288ccb9adbccc5f24507e1742e2
kim dong kyung <kdkd422@naver.com>
2024-12-31 17:57:30 +0900

    Feat 신청하기버튼 -> 게스트 상태에 따라 텍스트 및 호출함수 변경

--- BasketballGuest/BasketballGuest/Features/Guest/View/GuestDetail/PostApplyButton.swift
+++ BasketballGuest/BasketballGuest/Features/Guest/View/GuestDetail/PostApplyButton.swift
@@ -8,11 +8,106 @@
 import SwiftUI
 
 struct PostApplyButton: View {
+    
+    let loadState: LoadState
+    let guestStatus: GuestStatus
+    let error: () -> Void
+    let action: (GuestStatus) -> Void
+    
+    init(loadState: LoadState, guestStatus: GuestStatus, error: @escaping () -> Void, action: @escaping (GuestStatus) -> Void) {
+        self.loadState = loadState
+        self.guestStatus = guestStatus
+        self.error = error
+        self.action = action
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            switch loadState {
+            case .none, .loading:
+                LoadButton()
+            case .completed:
+                ApplyButton(guestStatus: guestStatus) {
+                    action(guestStatus)
+                }
+            case .failed:
+                ErrorButton() {
+                    error()
+                }
+            }
+        }
+    }
+}
+
+struct LoadButton: View {
+    var body: some View {
+        Button {} label: {
+            ProgressView()
+                .modifier(ApplyButtonModifier())
+        }
+        .disabled(true)
+    }
+}
+
+struct ApplyButton: View {
+    let guestStatus: GuestStatus
+    let action: () -> Void
+    
+    init(guestStatus: GuestStatus, action: @escaping () -> Void) {
+        self.guestStatus = guestStatus
+        self.action = action
+    }
+    
+    var body: some View {
+        VStack {
+            switch guestStatus {
+            case .owner:
+                ApplyButtonView(text: "신청자 관리", action: action)
+            case .guest:
+                ApplyButtonView(text: "게스트 취소", action: action)
+            case .apply:
+                ApplyButtonView(text: "신청취소", action: action)
+            case .rejected, .none:
+                ApplyButtonView(text: "신청하기", action: action)
+            }
+        }
+    }
+}
+
+struct ErrorButton: View {
+    let action: () -> Void
+    
+    init(action: @escaping () -> Void) {
+        self.action = action
+    }
+    
+    var body: some View {
+        Button(action: action) {
+            Text("다시 시도")
+                .modifier(ApplyButtonModifier())
+        }
+    }
+}
+
+struct ApplyButtonView: View {
+    let text: String
+    let action: () -> Void
+    
+    init(text: String, action: @escaping () -> Void) {
+        self.text = text
+        self.action = action
+    }
+    var body: some View {
+        Button(action: action) {
+            Text(text)
+                .modifier(ApplyButtonModifier())
+        }
     }
 }
 
 #Preview {
-    PostApplyButton()
+    PostApplyButton(loadState: .loading, guestStatus: .apply) {
+        
+    } action: { status in
+    }
 }
