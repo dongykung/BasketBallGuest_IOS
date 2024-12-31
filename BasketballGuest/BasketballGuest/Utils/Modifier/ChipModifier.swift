973e9ebadaf6d0ac48b338e8390491cc359de03f
kim dong kyung <kdkd422@naver.com>
2024-12-31 17:55:43 +0900

    Add Chip모디파이어

--- BasketballGuest/BasketballGuest/Utils/Modifier/ChipModifier.swift
+++ BasketballGuest/BasketballGuest/Utils/Modifier/ChipModifier.swift
@@ -5,4 +5,17 @@
 //  Created by 김동경 on 12/30/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ChipModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .foregroundStyle(.accent)
+            .font(.semibold12)
+            .padding(.vertical, 8)
+            .padding(.horizontal, 12)
+            .background(.chipbackground)
+            .clipShape(.rect(cornerRadius: 16))
+    }
+}
+
