afdba7b6388e2ba87addc2a300d79ee238079450
kim dong kyung <kdkd422@naver.com>
2024-12-31 17:56:22 +0900

    Feat 게스트 관리 기능

--- BasketballGuest/BasketballGuest/Features/Guest/View/GuestDetail/GuestAdminView.swift
+++ BasketballGuest/BasketballGuest/Features/Guest/View/GuestDetail/GuestAdminView.swift
@@ -8,11 +8,168 @@
 import SwiftUI
 
 struct GuestAdminView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    @StateObject private var viewModel: GuestAdminViewModel = GuestAdminViewModel()
+    @State private var isEmissionAlert: Bool = false
+    @State private var emissionName: String = ""
+    @State private var emissionUserId: String = ""
+    let postId: String
+    
+    init(postId: String) {
+        self.postId = postId
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            ApplyTopBar() {
+                dismiss()
+            }
+            switch viewModel.loadState {
+            case .none, .loading:
+                LoadingView()
+            case .completed:
+                applyList
+            case .failed:
+                ErrorView {
+                    viewModel.observeGuestAdmin(postId: postId)
+                }
+            }
+        }
+        .toastView(toast: $viewModel.toast)
+        .onAppear {
+            viewModel.observeGuestAdmin(postId: postId)
+        }
+        .alert(isPresented: $isEmissionAlert) {
+            let cancelButton = Alert.Button.destructive(Text("방출하기")) {
+                Task {
+                    await viewModel.emissionApplyGuest(postId: postId, userId: emissionUserId, nickName: emissionName)
+                }
+            }
+            let emissionButton = Alert.Button.cancel(Text("취소")) {
+                //print("secondary button pressed")
+            }
+            return Alert(title: Text("게스트 방출"), message: Text("\(emissionName)님을 방출하겠습니까?"), primaryButton: cancelButton, secondaryButton: emissionButton)
+        }
+    }
+    
+    @ViewBuilder
+    var applyList: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(viewModel.managementGuest, id: \.status.documentId) { applyStatus in
+                    if applyStatus.status.status != .rejected {
+                        ApplyItem(
+                            viewModel: viewModel,
+                            applyStatus: applyStatus,
+                            postId: postId
+                        ) {
+                            emissionName = applyStatus.user.nickName
+                            emissionUserId = applyStatus.user.id ?? ""
+                            isEmissionAlert.toggle()
+                        }
+                    }
+                }
+            }
+        }
+        .animation(.smooth, value: viewModel.managementGuest.count)
+    }
+}
+
+struct ApplyTopBar: View {
+    let action: () -> Void
+    
+    init(action: @escaping () -> Void) {
+        self.action = action
+    }
+    
+    var body: some View {
+        HStack {
+            Button(action: action ) {
+                Image(systemName: "chevron.left")
+                    .font(.title2)
+                    .foregroundStyle(.basic)
+                    .bold()
+            }
+            Spacer()
+        }
+        .overlay {
+            Text("신청자 관리")
+                .font(.semibold16)
+        }
+        .padding()
+    }
+}
+
+struct ApplyItem: View {
+    @ObservedObject var viewModel: GuestAdminViewModel
+    let applyStatus: GuestApplyStatus
+    let postId: String
+    let action: () -> Void
+    var body: some View {
+        HStack {
+            DefaultProfileView(profileUrl: applyStatus.user.profileImageUrl, frame: 60)
+            Text(applyStatus.user.nickName)
+            if applyStatus.status.status == .guest {
+                Text("게스트")
+                    .modifier(ChipModifier())
+            }
+            Spacer()
+            switch applyStatus.status.status {
+            case .apply, .none:
+                confirmWithCancel
+            case .guest:
+                guestCancel
+            default:
+                EmptyView()
+            }
+        }
+        .padding()
+    }
+    
+    @ViewBuilder
+    var confirmWithCancel: some View {
+        HStack {
+            Button {
+                Task {
+                    await viewModel.approveApplyGuest(postId: postId, userId: applyStatus.user.id ?? "", nickName: applyStatus.user.nickName)
+                }
+            } label: {
+                Text("승인")
+                    .foregroundStyle(.blue)
+                    .padding(8)
+                    .background(Color(uiColor: .systemGray6))
+                    .clipShape(.rect(cornerRadius: 12))
+            }
+            .padding(.trailing, 4)
+            
+            Button {
+                Task {
+                    await viewModel.rejectApplyGuest(postId: postId, userId: applyStatus.user.id ?? "", nickName: applyStatus.user.nickName)
+                }
+            } label: {
+                Text("거절")
+                    .foregroundStyle(.red)
+                    .padding(8)
+                    .background(Color(uiColor: .systemGray6))
+                    .clipShape(.rect(cornerRadius: 12))
+            }
+        }
+        .padding()
+    }
+    
+    @ViewBuilder
+    var guestCancel: some View {
+        Button(action: action) {
+            Text("방출")
+                .foregroundStyle(.red)
+                .padding(8)
+                .background(Color(uiColor: .systemGray6))
+                .clipShape(.rect(cornerRadius: 12))
+        }
     }
 }
 
 #Preview {
-    GuestAdminView()
+    GuestAdminView(postId: "")
 }
--- BasketballGuest/BasketballGuest/Features/Guest/ViewModel/GuestDetail/GuestAdminViewModel.swift
+++ BasketballGuest/BasketballGuest/Features/Guest/ViewModel/GuestDetail/GuestAdminViewModel.swift
@@ -5,4 +5,123 @@
 //  Created by 김동경 on 12/30/24.
 //
 
+import Factory
 import Foundation
+import FirebaseFirestore
+
+class GuestAdminViewModel: ObservableObject {
+    
+    @Injected(\.userService) private var userService
+    @Injected(\.guestDetailService) private var guestDetailService
+    
+    @Published var managementGuest: [GuestApplyStatus] = []
+    @Published var loadState: LoadState = .none
+    @Published var toast: Toast?
+    private let db = Firestore.firestore()
+    
+    func observeGuestAdmin(postId: String) {
+        loadState = .loading
+        db.collection("Guest").document(postId).collection("GuestStatus").addSnapshotListener { [weak self] querySnapshot, error in
+            guard let snapshot = querySnapshot else {
+                DispatchQueue.main.async {
+                    self?.loadState = .completed
+                }
+                return
+            }
+            if let _ = error {
+                DispatchQueue.main.async {
+                    self?.loadState = .failed
+                }
+            }
+            DispatchQueue.main.async {
+                self?.loadState = .completed
+            }
+            snapshot.documentChanges.forEach { diff in
+                do {
+                    let userStatus = try diff.document.data(as: UserStatus.self)
+                    switch diff.type {
+                    case .added:
+                        self?.addHandleGuest(userStatus: userStatus)
+                    case .modified:
+                        self?.updateHandleGuest(userStatus: userStatus)
+                    case .removed:
+                        self?.removeHandleGuest(userStatus: userStatus)
+                    }
+                } catch {
+                    
+                }
+            }
+        }
+    }
+    
+    private func addHandleGuest(userStatus: UserStatus) {
+        guard let userUid = userStatus.documentId else { return }
+        Task {
+            do {
+                let userData = try await userService.fetchUserData(userId: userUid)
+                let data = GuestApplyStatus(user: userData, status: userStatus)
+                await addManagementData(data)
+            } catch {
+                
+            }
+        }
+    }
+    
+    private func updateHandleGuest(userStatus: UserStatus) {
+        if let index = managementGuest.firstIndex(where: { $0.status.documentId == userStatus.documentId}) {
+            var newItem = managementGuest[index]
+            newItem.status = userStatus
+            managementGuest[index] = newItem
+        }
+    }
+    
+    private func removeHandleGuest(userStatus: UserStatus) {
+        if let index = managementGuest.firstIndex(where: { $0.status.documentId == userStatus.documentId }) {
+            toast = Toast(style: .warning, message: "\(managementGuest[index].user.nickName)님이 게스트 신청을 취소했습니다.")
+            managementGuest.remove(at: index)
+        }
+    }
+    
+    func approveApplyGuest(postId: String, userId: String, nickName: String) async {
+        do {
+            try await guestDetailService.approveApplyGuest(postId: postId, userId: userId)
+            await showToast(msg: "\(nickName)님을 게스트로 승인했습니다.", style: .success)
+        } catch {
+            await showToast(msg: "승인에 실패했습니다 다시 시도해 주세요.", style: .warning)
+        }
+    }
+    
+    func rejectApplyGuest(postId: String, userId: String, nickName: String) async {
+        do {
+            try await guestDetailService.rejectApplyGuest(postId: postId, userId: userId)
+            await showToast(msg: "\(nickName)님을 거절했습니다.", style: .success)
+        } catch {
+            await showToast(msg: "거절에 실패했습니다 다시 시도해 주세요.", style: .warning)
+        }
+    }
+    
+    func emissionApplyGuest(postId: String, userId: String, nickName: String) async {
+        do {
+            try await guestDetailService.emissionApplyGuest(postId: postId, userId: userId)
+            await showToast(msg: "\(nickName)님을 방출했습니다.", style: .success)
+        } catch{
+            await showToast(msg: "방출에 실패했습니다 디시 시도해 주세요.")
+        }
+    }
+    
+    @MainActor
+    func addManagementData(_ data: GuestApplyStatus) {
+        managementGuest.append(data)
+    }
+    
+    @MainActor
+    func showToast(msg: String, style: ToastStyle = .warning) {
+        toast = Toast(style: style, message: msg)
+    }
+}
+
+
+struct GuestApplyStatus {
+    var user: UserDTO
+    var status: UserStatus
+}
