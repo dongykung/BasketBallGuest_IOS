61427cedd2c459cbbaf51d7c9831d5f8ea68545f
kim dong kyung <kdkd422@naver.com>
2024-12-31 17:55:26 +0900

    Add 신청하기 버튼 모디파이어

--- BasketballGuest/BasketballGuest/Utils/Modifier/ApplyButtonModifier.swift
+++ BasketballGuest/BasketballGuest/Utils/Modifier/ApplyButtonModifier.swift
@@ -5,4 +5,17 @@
 //  Created by 김동경 on 12/30/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct ApplyButtonModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .frame(maxWidth: .infinity)
+            .font(.semibold18)
+            .foregroundStyle(.white)
+            .background(.accent)
+            .clipShape(.rect(cornerRadius: 12))
+            .padding()
+    }
+}
