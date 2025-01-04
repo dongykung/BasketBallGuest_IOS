8f2c0ee8d5a2ef6e812fbf4852c68dc6761f5227
kim dong kyung <kdkd422@naver.com>
2025-01-04 21:30:32 +0900

    project.pbxproj

--- BasketballGuest/BasketballGuest/Utils/Keyboard/KeyboardResponder.swift
+++ BasketballGuest/BasketballGuest/Utils/Keyboard/KeyboardResponder.swift
@@ -5,4 +5,31 @@
 //  Created by 김동경 on 12/27/24.
 //
 
-import Foundation
+import SwiftUI
+import Combine
+
+final class KeyboardResponder: ObservableObject {
+    @Published var currentHeight: CGFloat = 0
+    
+    private var cancellables: Set<AnyCancellable> = []
+    
+    init() {
+        let keyboardWillShow = NotificationCenter.default.publisher(for: UIResponder.keyboardWillShowNotification)
+            .compactMap { notification -> CGFloat? in
+                if let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+                    return keyboardFrame.height
+                }
+                return nil
+            }
+        
+        let keyboardWillHide = NotificationCenter.default.publisher(for: UIResponder.keyboardWillHideNotification)
+            .map { _ -> CGFloat in
+                return 0
+            }
+        
+        Publishers.Merge(keyboardWillShow, keyboardWillHide)
+            .subscribe(on: RunLoop.main)
+            .assign(to: \.currentHeight, on: self)
+            .store(in: &cancellables)
+    }
+}
